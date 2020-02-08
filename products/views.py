1a7c8f74b8e116ad1a0432e9a65bc78c25ffd900
Ubuntu <ubuntu@ip-172-31-84-209.ec2.internal>
2020-02-08 14:24:23 +0000

    added view to product list

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse, redirect, reverse, get_object_or_404
-
+from .models import Product
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def home(request):
@@ -10,3 +11,20 @@ def about(request):
     
 def contact(request):
     return render(request, 'contact.html')
+    
+def product_list(request):
+    products = Product.objects.all();
+    
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(products, 9)
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+
+    return render(request, 'product_list.html',{
+        'products':products
+    })
