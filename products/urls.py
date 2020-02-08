3612dd3a19edb303974d19732bb3515ac07d1752
Ubuntu <ubuntu@ip-172-31-84-209.ec2.internal>
2020-02-08 14:26:34 +0000

    created product views urls

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import home
+from .views import home, product_list, search_products, product_details, filter_products
 
 urlpatterns = [
     path('', home, name="index"),
+    path('products/', product_list, name="products"),
+    path('result/', search_products, name="search"),
+    path('details/<id>', product_details, name="details"),
+    path('filter/<category>', filter_products, name="filter"),
 ]
