ba4f828345b0fae6f244bf0a1fb3c335772bdd3c
Ubuntu <ubuntu@ip-172-31-84-209.ec2.internal>
2020-02-08 14:54:29 +0000

    added registration form

--- accounts/forms.py
+++ accounts/forms.py
@@ -13,3 +13,69 @@ class UserLoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
     
+class UserRegistrationForm(UserCreationForm):
+    """Form used to register a new user"""
+
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label="Password Confirmation",
+        widget=forms.PasswordInput)
+    first_name = forms.CharField(
+        label="First Name",
+        widget=forms.TextInput)
+    last_name = forms.CharField(
+        label="Last Name",
+        widget=forms.TextInput)
+        
+    class Meta:
+        model = MyUser
+        fields = ['first_name', 'last_name', 'email','username', 'password1', 'password2']
+        
+    def clean_email(self):
+        User = get_user_model()
+        email = self.cleaned_data.get('email') #1
+        username = self.cleaned_data.get('username')
+        #2 check if the email is unique, using the Django ORM
+        if User.objects.filter(email=email):
+            raise forms.ValidationError('Your email address must be unique')
+        return email
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
+        if not password1 or not password2:
+            raise ValidationError("Please confirm your password")
+        
+        if password1 != password2:
+            raise ValidationError("Passwords must match")
+        
+        return password2
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Row(
+                Column('email', css_class='form-group col-md-6 mb-0'),
+                Column('username', css_class='form-group col-md-6 mb-0'),
+                css_class='form-row'
+            ),
+
+            Row(
+                Column('first_name', css_class='form-group col-md-6 mb-0'),
+                Column('last_name', css_class='form-group col-md-6 mb-0'),
+                css_class='form-row'
+            ),
+            Row(
+                Column('password1', css_class='form-group col-md-6 mb-0'),
+                Column('password2', css_class='form-group col-md-6 mb-0'),
+                css_class='form-row'
+            ),
+            
+            Submit('submit', 'Register')
+          )
+          
+          
