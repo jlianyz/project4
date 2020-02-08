63285b431585cec53694a1af50a6b52292ead942
Ubuntu <ubuntu@ip-172-31-84-209.ec2.internal>
2020-02-08 14:56:11 +0000

    added log in view

--- accounts/views.py
+++ accounts/views.py
@@ -15,3 +15,28 @@ def logout(request):
     auth.logout(request)
     messages.success(request, "You have successfully been logged out")
     return redirect(reverse('main'))
+
+def login(request):
+    """Returns the login page"""
+    if request.method == 'POST':
+        login_form = UserLoginForm(request.POST) # populate the form from what the user has keyed in
+        if login_form.is_valid():
+            # attempt to check the username and password is valid
+            user = auth.authenticate(username=request.POST['username'],
+                                     password=request.POST['password'])
+            messages.success(request, "You have successfully logged in")
+            if user:
+                # log in the user
+                auth.login(user=user, request=request)
+                return redirect(reverse('main'))
+            else:
+                login_form.add_error(None, "Invalid username or password")
+                return render(request, 'user_login.html', {
+                  'form':login_form
+                })
+
+    else:
+        login_form = UserLoginForm()
+        return render(request, 'user_login.html', {
+            'form':login_form
+        })
