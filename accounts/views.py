1cc742a1f6b42e3cef19a530e31fbc35653a17f2
Ubuntu <ubuntu@ip-172-31-84-209.ec2.internal>
2020-02-08 14:55:47 +0000

    added main account and logout views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+from .models import MyUser
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth, messages
+from django.shortcuts import render, HttpResponse, redirect, reverse, get_object_or_404
+from .forms import UserLoginForm, UserRegistrationForm, UpdateDetailsForm
+from django.template.context_processors import csrf
 
 # Create your views here.
+def main_account(request):
+    return render(request, 'main_acct.html')
+    
+def logout(request):
+    auth.logout(request)
+    messages.success(request, "You have successfully been logged out")
+    return redirect(reverse('main'))
