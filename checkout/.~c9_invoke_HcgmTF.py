c9de8493759c7d265d48ba56c26821a9fb8ab74e
Ubuntu <ubuntu@ip-172-31-84-209.ec2.internal>
2020-02-09 03:38:06 +0000

    regstered checkout models in admin

---
+++ checkout/.~c9_invoke_HcgmTF.py
@@ -0,0 +1,59 @@
+from django.db import models
+from project4.settings import AUTH_USER_MODEL
+from products.models import Product
+from django.utils import timezone
+from decimal import Decimal
+from datetime import timedelta,datetime
+
+# Create your models here.
+class Charge(models.Model):
+    full_name = models.CharField(max_length=50, blank=False)
+    phone_number = models.CharField(max_length=20, blank=False)
+    country = models.CharField(max_length=40, blank=False)
+    postcode = models.CharField(max_length=20, blank=True)
+    town_or_city = models.CharField(max_length=40, blank=False)
+    street_address1 = models.CharField(max_length=40, blank=False)
+    street_address2 = models.CharField(max_length=40, blank=False)
+    date = models.DateField()
+
+    def __str__(self):
+        return "{0}-{1}-{2}".format(self.id, self.date, self.full_name)
+
+class Transaction(models.Model):
+    
+    status_options = [
+        ('pending', "Pending"),
+        ('approved', "Approved"),
+        ('rejected', "Rejected"),
+        ('shipping', "Shipping"),
+        ('delivered', "Delivered"),
+        ('lost', "Lost")
+        
+    ]
+    
+    charge = models.ForeignKey('Charge',on_delete=models.CASCADE,null=True)
+    status = models.CharField(blank=False,choices=status_options,max_length=50)
+    date = models.DateField()
+    owner= models.ForeignKey("accounts.MyUser", on_delete=models.SET_NULL,null=True)
+    
+    def subtotal_cost(self):
+        return '{0:.2f}'.format(sum(item.cost() for item in self.items.all()))
+
+    def delivery_fee(self):
+        if Decimal(self.subtotal_cost()) > Decimal(25.00):
+            return '{0:.2f}'.format(0)
+        else:
+            return '{0:.2f}'.format(4.90)
+
+    def total_cost(self):
+        return '{0:.2f}'.format(Decimal(self.subtotal_cost()) + Decimal(self.delivery_fee()))
+
+class LineItem(models.Model):
+    product = models.ForeignKey('products.product', null=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255,blank=False)
+    price = models.IntegerField(blank=False)
+    transaction = models.ForeignKey('Transaction',on_delete= models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    
+    def cost(self):
+        return self.price * self.quantity
--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Charge, Transaction,LineItem
 # Register your models here.
+
+admin.site.register(Charge)
+admin.site.register(Transaction)
+admin.site.register(LineItem)
