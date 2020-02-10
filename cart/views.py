a4c1d299ba71b3ff0767548a72e3886317d04154
Ubuntu <ubuntu@ip-172-31-84-209.ec2.internal>
2020-02-10 15:02:23 +0000

    updated cart html

--- cart/views.py
+++ cart/views.py
@@ -3,14 +3,17 @@ from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from .models import CartProduct
 
+
 # Create your views here.
 def view_cart(request):
-    cart = request.session.get('cart', {})
-    # reminder: request.user is the currently logged in user
-    cart = CartProduct.objects.filter(owner=request.user)
-    return render(request, 'view_cart.html',{
-        'cart':cart
-    })
+    if request.user:
+       # reminder: request.user is the currently logged in user
+       # cart = CartProduct.objects.filter(owner=request.user)
+        return render(request, 'view_cart.html',{
+            # 'cart':cart
+        })
+    #else:
+    #    return(render('template')
     
 def add_to_cart(request, id):
     """Add a quantity of the specified product to the cart"""
